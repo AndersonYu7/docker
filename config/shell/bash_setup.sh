6826fb480d12f22826a1d6bf35b761bf48ed2b2c
AndersonYu7 <andersonyu614@gmail.com>
2025-05-18 14:46:12 +0800

    update bash_setup.sh -> add [🐳]

--- config/shell/bash_setup.sh
+++ config/shell/bash_setup.sh
@@ -25,7 +25,11 @@ parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+    if [ -n "$DOCKER_NAME" ]; then
+        PS1="\[\033[01;36m\][🐳 $DOCKER_NAME]\[\033[00m\]${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\]\$ "
+    else
+        PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+    fi
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
 fi
